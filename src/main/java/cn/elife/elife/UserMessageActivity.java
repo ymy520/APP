bbd89638e44f9dd091118477f9d038f4d4de160a
kai <kai@163.com>
2016-05-21 13:04:51 +0800

    完成了测试R功能

--- src/main/java/cn/elife/elife/UserMessageActivity.java
+++ src/main/java/cn/elife/elife/UserMessageActivity.java
@@ -1,30 +1,29 @@
 package cn.elife.elife;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
-import android.preference.DialogPreference;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-
 public class UserMessageActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String TAG = "usermessage";
     private View headView;
     private View nameView;
     private View sexView;
     private View passwordView;
     private View emailView;
     private View codeView;
-    private ImageView myHeadImage;
+    private TextView modifySex;
+    private TextView modifyNameText;
+    private TextView modifyEmailText;
     AlertDialog.Builder dialogBuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,25 +45,31 @@ public class UserMessageActivity extends AppCompatActivity implements View.OnCli
         passwordView=findViewById(R.id.user_mypassword);
         emailView=findViewById(R.id.user_myemail);
         codeView=findViewById(R.id.user_mycode);
+        modifySex= (TextView) findViewById(R.id.user_mysex1);
+        modifyNameText= (TextView) findViewById(R.id.user_myname1);
+        modifyEmailText= (TextView) findViewById(R.id.user_myemail1);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.user_myhead:
+                Log.e(TAG,""+v.getId());
                 modifyMyHead();
                 break;
             case R.id.user_myname:
+                Log.e(TAG,""+v.getId());
                 modifyMyName();
                 break;
             case R.id.user_mysex:
+                Log.e(TAG,""+v.getId());
                 modifySex();
                 break;
             case R.id.user_mypassword:
-
+                modifyPassword();
                 break;
             case R.id.user_myemail:
-
+                modifyEmail();
                 break;
             case R.id.user_mycode:
 
@@ -72,20 +77,51 @@ public class UserMessageActivity extends AppCompatActivity implements View.OnCli
         }
     }
 
+    private void modifyPassword() {
+        Intent intent=new Intent(UserMessageActivity.this,UsermessagePasswordActivity.class);
+        startActivity(intent);
+    }
+
+    private void modifyEmail() {
+        final EditText et=new EditText(this);
+        //设置用户邮箱的方法
+        new AlertDialog.Builder(this).setTitle("请输入用户邮箱").setView(
+                et).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // 点击确定按钮后得到输入的值，保存
+                String str=et.getText().toString();
+                modifyEmailText.setText(str);
+            }
+        }).setNegativeButton("取消", null).show();
+    }
+
     private void modifySex() {
         dialogBuilder=new AlertDialog.Builder(this);
         dialogBuilder.setTitle("修改性别");
         final String[] items={"男","女"};
-        dialogBuilder.setSingleChoiceItems(items, 1, new DialogInterface.OnClickListener() {
+        dialogBuilder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(UserMessageActivity.this, "你选择了："+items, Toast.LENGTH_SHORT).show();
+            public void onClick(DialogInterface dialog, int i) {
+                Toast.makeText(UserMessageActivity.this, "你选择了："+items[i], Toast.LENGTH_SHORT).show();
+                modifySex.setText(items[i]);
             }
         });
+        dialogBuilder.setCancelable(true);
+        AlertDialog dialog=dialogBuilder.create();
+        dialog.show();
     }
 
     private void modifyMyName() {
-
+        final EditText et=new EditText(this);
+        //设置用户名的方法
+        new AlertDialog.Builder(this).setTitle("请输入新用户名").setView(
+                et).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // 点击确定按钮后得到输入的值，保存
+                String str=et.getText().toString();
+                modifyNameText.setText(str);
+            }
+        }).setNegativeButton("取消", null).show();
     }
 
     private void modifyMyHead() {
