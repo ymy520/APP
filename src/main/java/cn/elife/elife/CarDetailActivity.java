bf3d87a5d00569de0a028d031380298bf40f1d34
kai <kai@163.com>
2016-05-26 21:52:06 +0800

    完成洗车详情界面

--- src/main/java/cn/elife/elife/CarDetailActivity.java
+++ src/main/java/cn/elife/elife/CarDetailActivity.java
@@ -1,13 +1,63 @@
 package cn.elife.elife;
 
+import android.support.percent.PercentRelativeLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class CarDetailActivity extends AppCompatActivity {
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.elife.adapters.CarMerchAdapter;
+import cn.elife.bean.CarEVContent;
+import cn.elife.bean.CarEVNum;
+import cn.elife.bean.CarMerchantHead;
+
+public class CarDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView carBack;
+    private ListView carMerchanListView;
+    private List<Object> mList;
+    private PercentRelativeLayout carWriteOrder;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_detail);
+        initViews();
+        initData();
+        CarMerchAdapter carMerchAdapter=new CarMerchAdapter(CarDetailActivity.this,mList);
+        carMerchanListView.setAdapter(carMerchAdapter);
+        carWriteOrder.setOnClickListener(this);
+    }
+
+    private void initData() {
+        CarMerchantHead merchantHead=new CarMerchantHead(R.drawable.car1,R.drawable.car2,R.drawable.car3,"江苏省","18825486923");
+        mList.add(merchantHead);
+        CarEVNum carEVNum=new CarEVNum(33);
+        mList.add(carEVNum);
+        for(int i=1;i<=20;i++) {
+            CarEVContent carEVContent = new CarEVContent("悠悠", "2016-05-"+i,"服务非常好",1);
+            mList.add(carEVContent);
+        }
+    }
+
+    private void initViews() {
+        carBack = (ImageView) findViewById(R.id.car_back);
+        carMerchanListView = (ListView) findViewById(R.id.car_merchan_listView);
+        mList=new ArrayList<>();
+        carWriteOrder = (PercentRelativeLayout) findViewById(R.id.car_write_order);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(CarDetailActivity.this, "跳转到订单页面", Toast.LENGTH_SHORT).show();
     }
 }
--- src/main/java/cn/elife/utils/CarDetailViewPager.java
+++ src/main/java/cn/elife/utils/CarDetailViewPager.java
@@ -1,16 +1,19 @@
 package cn.elife.utils;
 
 import android.content.Context;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 
 /**
  * Author：张凯  on 2016/5/25 21:30
  * Blog: bukevin@github.io
- *
+ * <p/>
  * 此类用于洗车商家详情页的轮播图播放功能
  */
-public class CarDetailViewPager extends ViewPager{
+public class CarDetailViewPager extends ViewPager {
+
+
     public CarDetailViewPager(Context context) {
         super(context);
     }
@@ -18,49 +21,139 @@ public class CarDetailViewPager extends ViewPager{
     public CarDetailViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-    //播放时间
-    private int showTime=3*1000;
-    //滚动方向
-    private Direction direction=Direction.LEFT;
+
+    /**
+     * 播放时间
+     */
+    private int showTime = 3 * 1000;
+    /**
+     * 滚动方向
+     */
+    private Direction direction = Direction.LEFT;
 
     /**
      * 设置播放时间，默认3秒
-     * @param showTimeMillis
+     *
+     * @param showTimeMillis 毫秒
      */
-    public void setShowTime(int showTimeMillis){
-        this.showTime=showTime;
+    public void setShowTime(int showTimeMillis) {
+        this.showTime = showTime;
     }
 
     /**
-     * 设置滚动方向，默认向左
-     * @param direction
+     * 设置滚动方向，默认向左滚动
+     *
+     * @param direction 方向
      */
-    public void setDirection(Direction direction){
-        this.direction=direction;
-    }
-
-    public void start(){
-        //开始播放
-    }
-
-    public void stop(){
-        //停止播放
-    }
-
-    public void previous(){
-        //播放上一个
-    }
-
-    public void next(){
-        //播放下一个
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
 
     /**
-     * 用于指定轮播图播放方向
+     * 开始
      */
-    public enum Direction{
-        LEFT,RIGHT
+    public void start() {
+        stop();
+        postDelayed(player, showTime);
     }
+
+    /**
+     * 停止
+     */
+    public void stop() {
+        removeCallbacks(player);
+    }
+
+    /**
+     * 播放上一个
+     */
+    public void previous() {
+        if (direction == Direction.RIGHT) {
+            play(Direction.LEFT);
+        } else if (direction == Direction.LEFT) {
+            play(Direction.RIGHT);
+        }
+    }
+
+    /**
+     * 播放下一个
+     */
+    public void next() {
+        play(direction);
+    }
+
+    /**
+     * 执行播放
+     *
+     * @param direction 播放方向
+     */
+    private synchronized void play(Direction direction) {
+        PagerAdapter pagerAdapter = getAdapter();
+        if (pagerAdapter != null) {
+            int count = pagerAdapter.getCount();
+            int currentItem = getCurrentItem();
+            switch (direction) {
+                case LEFT:
+                    currentItem++;
+                    if (currentItem > count)
+                        currentItem = 0;
+                    break;
+                case RIGHT:
+                    currentItem--;
+                    if (currentItem < 0)
+                        currentItem = count;
+                    break;
+            }
+            setCurrentItem(currentItem);
+        }
+        start();
+    }
+
+    /**
+     * 播放器
+     */
+    private Runnable player = new Runnable() {
+        @Override
+        public void run() {
+            play(direction);
+        }
+    };
+
+    public enum Direction {
+        /**
+         * 向左行动，播放的应该是右边的
+         */
+        LEFT,
+
+        /**
+         * 向右行动，播放的应该是左边的
+         */
+        RIGHT
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        addOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == SCROLL_STATE_IDLE)
+                    start();
+                else if (state == SCROLL_STATE_DRAGGING)
+                    stop();
+            }
+        });
+    }
+
 }
 
 
